e4e4d19c9515279ae4d4765fe4434518ec0265d6
Angelo7x <abennici96@gmail.com>
2021-09-08 17:25:46 +0200

    up grafica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 var numQuadrati = prompt("Inserisci il numero di quadrati da generare");
+
+function nuovoCampo(celle) {
+    for (i = 1; i <= celle; i++) {
+        document.getElementById("campo").innerHTML += `<div class="quadrato">${i}</div>`;
+    }
+}
+
+// creazione campo
+
+nuovoCampo(numQuadrati);
+
+
+document.getElementById("campo").addEventListener("click",
+	function(event) {
+		event.target.classList.toggle("cliccato");
+	}
+);
